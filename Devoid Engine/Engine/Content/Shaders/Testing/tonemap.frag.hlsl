692e5b41a6f335fc93a21766b38b19c9010c7a36
MohamedAshiqRafeeqAhmed <142332745+TDX-Dev@users.noreply.github.com>
2026-03-14 11:48:52 +0530

    RG

--- Devoid Engine/Engine/Content/Shaders/Testing/tonemap.frag.hlsl
+++ Devoid Engine/Engine/Content/Shaders/Testing/tonemap.frag.hlsl
@@ -18,6 +18,44 @@ cbuffer Material : register(b2)
 Texture2D MAT_SceneColor : register(t0);
 SamplerState MAT_SceneColorSampler : register(s0);
 
+
+static const float3x3 ACESInputMat =
+{
+    { 0.59719, 0.35458, 0.04823 },
+    { 0.07600, 0.90834, 0.01566 },
+    { 0.02840, 0.13383, 0.83777 }
+};
+
+// ODT_SAT => XYZ => D60_2_D65 => sRGB
+static const float3x3 ACESOutputMat =
+{
+    { 1.60475, -0.53108, -0.07367 },
+    { -0.10208, 1.10813, -0.00605 },
+    { -0.00327, -0.07276, 1.07602 }
+};
+
+float3 RRTAndODTFit(float3 v)
+{
+    float3 a = v * (v + 0.0245786f) - 0.000090537f;
+    float3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
+    return a / b;
+}
+
+float3 ACESFitted(float3 color)
+{
+    color = mul(ACESInputMat, color);
+
+    // Apply RRT and ODT
+    color = RRTAndODTFit(color);
+
+    color = mul(ACESOutputMat, color);
+
+    // Clamp to [0, 1]
+    color = saturate(color);
+
+    return color;
+}
+
 float3 AgxLog(float3 x)
 {
     return log2(max(x, 1e-6));
@@ -59,13 +97,11 @@ float4 PSMain(PSInput input) : SV_TARGET
 {
     float3 hdr = MAT_SceneColor.Sample(MAT_SceneColorSampler, input.UV0).rgb;
 
-    float exposure = 0.1;
+    float exposure = 2;
     hdr *= exposure;
 
-    float3 color = TonemapAgX(hdr);
-    //color = AgxLookPunchy(color);
-
-    color = pow(color, 1.0 / 2.2);
+    float3 color = ACESFitted(hdr);
+    //color = pow(color, 1.0 / 2.2);
 
     return float4(color, 1.0);
 }
